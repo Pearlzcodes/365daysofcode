aa3147a5e93cb8923f306f60f276ac2bdd82add1
Pearlzcodes <odeyemipelumi9@gmail.com>
2026-05-29 19:43:27 +0100

    Tic Tac Toe Logic

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -10,10 +10,21 @@ const box9 = document.getElementById('box9');
 const displayScreen = document.getElementById('displayscreen');
 const resetBtn = document.getElementById('reset-btn');
 const gameMode = document.getElementById('game-mode');
+const scoreDisplay = document.getElementById('scores')
 
 gameMode.addEventListener('change', resetGame);
 
+gameMode.addEventListener('change', () =>{
+playerOScore = 0;
+computerScore = 0;
+playerXScore = 0;
+scoreDisplay.textContent = "";
+})
 
+
+let playerXScore = 0;
+let computerScore = 0;
+let playerOScore = 0;
 
 
 
@@ -43,15 +54,91 @@ function resetGame() {
 resetBtn.addEventListener('click', resetGame)
 
 function computersTurn() {
-    const emptyBoxes = boxesArray.filter(box => box.textContent === "");
-    let boxIndex = Math.floor(Math.random() * emptyBoxes.length);
-    console.log(boxIndex)
-    emptyBoxes[boxIndex].textContent = "O";
-    emptyBoxes[boxIndex].style.color = 'red';
-    console.log(emptyBoxes[boxIndex]);
-    isX = true;
-    displayScreen.textContent = "It is Player X's turn";
-    checkForWinner();
+
+    for(let i = 0; i < winningArray.length; i++){
+        if(winningArray[i][0].textContent === "O" && winningArray[i][1].textContent === "O" && winningArray[i][2].textContent === "" && !isX){
+            winningArray[i][2].textContent = "O";
+            winningArray[i][2].style.color = "red";
+            goRandom = false;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Win Case 1")
+            return;
+        }
+
+        else if(winningArray[i][0].textContent === "O" && winningArray[i][1].textContent === "" && winningArray[i][2].textContent === "O" && !isX){
+            winningArray[i][1].textContent = "O";
+            winningArray[i][1].style.color = "red";
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Win Case 2")
+            return;
+        }
+
+        else if(winningArray[i][0].textContent === "" && winningArray[i][1].textContent === "O" && winningArray[i][2].textContent === "O" && !isX){
+            winningArray[i][0].textContent = "O";
+            winningArray[i][0].style.color = "red";
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Win Case 3")
+            return;
+        }
+
+    }
+
+    for(let i = 0; i < winningArray.length; i++){
+
+        if(winningArray[i][0].textContent === "X" && winningArray[i][1].textContent === "X" && winningArray[i][2].textContent === "" && !isX){
+            winningArray[i][2].textContent = "O";
+            winningArray[i][2].style.color = "red";
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Defense Case 1");
+            return;
+        }
+
+        else if(winningArray[i][0].textContent === "X" && winningArray[i][1].textContent === "" && winningArray[i][2].textContent === "X" && !isX){
+            winningArray[i][1].textContent = "O";
+            winningArray[i][1].style.color = "red";
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Defense Case 2")
+            return;
+        }
+
+        else if(winningArray[i][0].textContent === "" && winningArray[i][1].textContent === "X" && winningArray[i][2].textContent === "X" && !isX){
+            winningArray[i][0].textContent = "O";
+            winningArray[i][0].style.color = "red";
+            goRandom = false;
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Defense Case 3")
+            return;
+        }
+        
+        
+    }
+
+       
+            const emptyBoxes = boxesArray.filter(box => box.textContent === "");
+            let boxIndex = Math.floor(Math.random() * emptyBoxes.length);
+            console.log(boxIndex)
+            emptyBoxes[boxIndex].textContent = "O";
+            emptyBoxes[boxIndex].style.color = 'red';
+            console.log(emptyBoxes[boxIndex]);
+            isX = true;
+            displayScreen.textContent = "It is Player X's turn";
+            checkForWinner();
+            console.log("Played Randomly")
+            
+        
+
+
 }
 
 
@@ -76,15 +163,28 @@ function checkForWinner() {
         if (winningArray[i][0].textContent !== "" && winningArray[i][1].textContent === winningArray[i][0].textContent && winningArray[i][2].textContent === winningArray[i][1].textContent) {
             
             if(gameMode.value === '2players'){
-                displayScreen.textContent = `Player ${winningArray[i][0].textContent} wins`}
+                if(winningArray[i][0].textContent === "X"){
+                    playerXScore += 1;
+                    displayScreen.textContent = 'Player X wins'
+                    displayScreen.style.color = 'blue';
+                }
+                else{
+                    playerOScore += 1;
+                    displayScreen.textContent = 'Player O wins';
+                    displayScreen.style.color = 'red';
+                }
+
+                }
             else{
                 if(winningArray[i][0].textContent === "X"){
                     displayScreen.textContent = "Player X wins!"
                     displayScreen.style.color = 'blue';
+                    playerXScore += 1;
                 }
                 else{
                     displayScreen.textContent = "Computer wins!"
-                    displayScreen.style.color = 'red'
+                    displayScreen.style.color = 'red';
+                    computerScore += 1
                 }
             }
             isGaming = false;
@@ -120,7 +220,7 @@ if(isDraw && isGaming){
     displayScreen.textContent = "It's a draw!!!";
     isGaming = false;
 } */
-
+scoreDisplay.textContent = gameMode.value === '1player' ?  `Player X: ${playerXScore}....Computer: ${computerScore}` : `Player X: ${playerXScore}\n Player O: ${playerOScore}`
 
 }
 
